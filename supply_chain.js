44e61b3602f574998452fc30ae87aa0d70323812
HienNguyen311 <hiennguyenthi979@gmail.com>
2018-01-08 16:53:47 +0700

    update_contract_and

--- supply_chain.js
+++ supply_chain.js
@@ -1,6 +1,7 @@
 pragma solidity ^ 0.4 .15;
 
 contract Database {
+
   // mang dia chi product ma database nay luu tru
   address[] public products;
 
@@ -26,53 +27,74 @@ contract Database {
 
   mapping(address => address[]) public productOfOwner;
 
+  //kiem tra co phai chu so huu db ko
   modifier onlyOwnerDB {
       if (msg.sender != ownerDB)
         revert();
       _;
-    }
+  }
 
-    function checkAccountRaw(address _account) returns (bool){
+  // lay dia chi chu db
+  function getOwnerDB() constant returns (address){
+        return ownerDB;
+  }
+
+  // chuyen quyen so huu db 
+  function transferOwnerDB(address _newOwnerDB) onlyOwnerDB{
+    ownerDB = _newOwnerDB;
+  }
+
+  // kiem tra account co quyen tao sp ms hay ko
+  function checkAccountRaw(address _account) returns (bool){
     for (uint i = 0; i < accountRaw.length ; i++) {
       if (_account == accountRaw[i]) {
-      return true;
+          return true;
       }
     }
     return false;
-    }
+  }
 
-    function checkAccount(address _account) returns (bool){
-      for (uint i = 0; i < accounts.length ; i++) {
-        if (_account == accounts[i]) {
-          return true;
-        }
+  // kiem tra account da co so huu sp hay chua
+  function checkAccount(address _account) returns (bool){
+    for (uint i = 0; i < accounts.length ; i++) {
+      if (_account == accounts[i]) {
+        return true;
       }
-      return false;
     }
+    return false;
+  }
 
+  // access cho 1 account co quyen tao sp tho hay ko
   function AddlistAccountRaw(address _account) onlyOwnerDB {
       accountRaw.push(_account);
   }
+
+  // them account vao danh sach account co so huu sp co trong he thong
   function AddlistAccount(address _account)  {
       accounts.push(_account);
   }
 
+  // dem so luong account co so huu sp trong he thong
   function getCountAccount() constant returns (uint){
         return accounts.length;
   }
 
+  // lay dia chi account theo id
   function getAddressAccount(uint idx) constant returns (address){
         return accounts[idx];
   }
 
+  // dem so luong account 
   function getCountAccountRaw() constant returns (uint){
         return accountRaw.length;
   }
 
+  // lay dia chi account co quyen tao nguyen lieu tho theo id
   function getAddressAccountRaw(uint idx) constant returns (address){
         return accountRaw[idx];
   }
 
+  // gom cac san pham co chung chu so huu
   function AddlistProductOfOwner(address _handler, address _pro){
       productOfOwner[_handler].push(_pro);
   }
@@ -82,10 +104,9 @@ contract Database {
        products.push(productAddress);
   }
 
-  function createProduct(bytes32 _name, address[] _parentProducts, bytes32 _unit, uint _amount, uint[] _ratio, address _handler) returns(address) {
-
-        return new Product(_name, _parentProducts, _unit, _amount, _ratio, _handler, this);
-
+  // tao sp moi
+  function createProduct(bytes32 _name, address[] _parentProducts, bytes32 _unit, uint _amount, uint[] _ratio, address _handler, uint _expirydateChild) returns(address) {
+        return new Product(_name, _parentProducts, _unit, _amount, _ratio, _handler, _expirydateChild, this);
   }
 
   // so product hien tai duoc luu trong contract database
@@ -93,14 +114,17 @@ contract Database {
         return products.length;
   }
 
+  // lay dia chi product
   function getAddressProduct(uint idx) constant returns (address){
         return products[idx];
   }
 
+  // dem so pro thuoc quyen so huu cua 1 account bat ky
   function getCountProductOfOwner(address _handler) constant returns (uint) {
         return productOfOwner[_handler].length;
   }
 
+  // lay dia chi pro theo id
   function getProductOfOwnerByAddress(address _handler, uint idx) constant returns (address) {
         return productOfOwner[_handler][idx];
   }
@@ -139,12 +163,20 @@ contract Product {
       _;
     }
 
+    // check quyen huy sp nao do khi nay sinh van de
+    modifier onlyAccessCancel {
+      if ((msg.sender != owner) && (msg.sender != Database(DATABASE_CONTRACT).getOwnerDB()))
+        revert();
+      _;
+    }
+
     // kiem tra no la nguyen lieu tho hay khong
     modifier onlyHaveChild {
       if (parentProducts.length != 0)
         revert();
       _;
     }
+
 
     // mang dia chi cha cua sp hien tai
     address[] public parentProducts;
@@ -162,17 +194,27 @@ contract Product {
 
     uint public amount;
 
+    uint public expirydate;
+
     uint[] public ratioPro;
+
 
     // mang cac hanh dong duoc thuc hien tren sp do
     Action[] public actions;
 
-    function Product(bytes32 _name, address[] _parentProducts, bytes32 _unit, uint _amount, uint[] _ratio, address handler, address _DATABASE_CONTRACT) {
+    function Product(bytes32 _name, address[] _parentProducts, bytes32 _unit, uint _amount, uint[] _ratio, address handler, uint _expirydate, address _DATABASE_CONTRACT) {
 
       name = _name;
       parentProducts = _parentProducts;
       unit = _unit;
       amount = _amount;
+      expirydate = _expirydate;
+
+      if (expirydate <= now){
+        revert();
+      }
+
+
       if (amount == 0) {
         revert();
       }
@@ -216,17 +258,17 @@ contract Product {
         revert();
     }
 
+    // tao hanh dong tren mot sp bat ky
+    function derive(bytes32 newProductsName, bytes32 unitChild, uint amountChild, uint ratioToChild, uint expirydateChild) notConsumed onlyOwner{ 
 
-    function derive(bytes32 newProductsName, bytes32 unitChild, uint amountChild, uint ratioToChild) notConsumed onlyOwner{ 
-
-        uint totalSpend = amountChild * ratioToChild;
-
-        if (amount < totalSpend){
+        if ((expirydateChild <= now) || (amount < totalSpend)) {
           revert();
         }
 
+        uint totalSpend = amountChild * ratioToChild;
+
         if (amount == totalSpend){
-          isConsumed=true;
+          isConsumed = true;
         }
 
         this.setAmount(this.getAmount() - totalSpend);
@@ -246,12 +288,11 @@ contract Product {
         uint[] memory ratio1 = new uint[](1);
         ratio1[0] = ratioToChild;
 
-        address newProduct1 = database.createProduct(newProductsName, parentProduct, unitChild, amountChild, ratio1, owner ); 
-        childProducts.push(newProduct1);
-        
+        address newProduct = database.createProduct(newProductsName, parentProduct, unitChild, amountChild, ratio1, owner, expirydateChild ); 
+        childProducts.push(newProduct);       
     }
 
-
+    // lay action
     function getAction(uint idx) constant returns (string, uint, uint) {
         Action storage a = actions[idx];
 
@@ -264,50 +305,60 @@ contract Product {
     //chuyen quyen so huu product cho ng khac
     function transferOwnership(address _newOwner, uint _amount) onlyOwner{
 
-        if(_newOwner == owner){
+        if((_newOwner == owner) || (amount < _amount) || (expirydate <= now)){
           revert();
         }
 
-        if (amount < _amount) {
-          revert();
-        }
-
-        if(amount == _amount) {
-
-          this.setConsumed(true);
-        }
-
+        bool check = false;
 
         Action memory action;
         action.description ="Tranfer to new Owner"; 
         action.timestamp = now;
         actions.push(action);
 
-    
-        address[] memory parentProduct1 = new address[](1);
-        parentProduct1[0] = this;
+        for (uint i = 0; i < parentProducts.length; i++){
+          Product pro = Product(parentProducts[i]);
+          if ((_newOwner == pro.getOwner()) && (pro.getName() == name)){
+            check = true;
+            pro.setAmount(pro.getAmount() + _amount);
+            this.setAmount(this.getAmount() - _amount);
+            if (this.getAmount() == 0) {
+              this.setConsumed(true);
+            }   
+            break;
+          }
+        }
+        if (check == false){       
+          address[] memory parentProduct = new address[](1);
+          parentProduct[0] = this;
+          uint[] memory ratio1 = new uint[](1);
+          ratio1[0] = 0;
 
-        uint[] memory ratio1 = new uint[](1);
-        ratio1[0] = 0;
+          this.setAmount(this.getAmount() - _amount);
 
-        this.setAmount(this.getAmount() - _amount);
+          if (this.getAmount() == 0) {
+              this.setConsumed(true);
+          }   
 
-        Database database = Database(DATABASE_CONTRACT);
-
-        address newProduct2 = database.createProduct( name, parentProduct1, unit, _amount, ratio1, _newOwner );  
-
-        childProducts.push(newProduct2);
-
-
+          address newProduct = Database(DATABASE_CONTRACT).createProduct( name, parentProduct, unit, _amount, ratio1, _newOwner, expirydate);  
+          childProducts.push(newProduct);             
+        }
+        
     }
     // get owner
     function getOwner() constant returns (address){
         return owner;
     }
 
+    // lay ten sp
+    function getName() constant returns (bytes32){
+        return name;
+    }
+
     function getCountRatioPro() constant returns (uint){
         return ratioPro.length;
     }
+
 
     function getRatioProByIdx(uint idx) constant returns (uint){
         return ratioPro[idx];
@@ -359,46 +410,41 @@ contract Product {
         if (isConsumed== true) this.setAmount(0); 
     }
 
+    function cancel() onlyAccessCancel{
+      this.setAmount(0);
+      this.setConsumed(true);
+    }
+
     // them so luong product neu da su dung het, ap dung voi nguyen lieu tho
     function setNewAmount(uint _newAmount) onlyHaveChild onlyOwner{
         if(_newAmount < 0){
           revert();
         }
         amount = _newAmount; 
+
         if(amount == 0){
           isConsumed = true;
-        }else{
+        }
+        else{
           isConsumed= false;
         }
     }
 
-    function merge( address[] otherProducts, bytes32 newProductName,uint[] ratioToProduct,uint newProductAmount, bytes32 newProductUnit) notConsumed  {
-
-        if ((otherProducts.length +1) != ratioToProduct.length )
-           revert();
-
-        //code them cho th xu ly phai la parent
-        address[] memory parentProduct1 = new address[](otherProducts.length+1);
-
-        for (uint i1 = 0; i1 < otherProducts.length; i1++) {
-          parentProduct1[i1] = otherProducts[i1];
+    // Ham ket hop nhieu sp thanh mot sp
+    function merge( address[] otherProducts, bytes32 newProductName,uint[] ratioToProduct,uint newProductAmount, bytes32 newProductUnit, uint expirydateChild) notConsumed  {
+        if ((expirydateChild <= now) || (otherProducts.length != ratioToProduct.length )) {
+          revert();
         }
 
-        parentProduct1[otherProducts.length] = this;
-
-        for (uint j1 = 0; j1 < parentProduct1.length; j1++) {
-
-          Product pro1 = Product(parentProduct1[j1]);
-          if(pro1.getOwner() != msg.sender) revert();
-          if (pro1.getAmount() < (newProductAmount * ratioToProduct[j1])) revert();     
+        for (uint i = 0; i < otherProducts.length; i++) {
+          Product pro = Product(otherProducts[i]);
+          if((pro.getOwner() != msg.sender) || (pro.getAmount() < (newProductAmount * ratioToProduct[i]))) revert(); 
         }
 
-        //Database database = Database(DATABASE_CONTRACT);
+        address newProduct = Database(DATABASE_CONTRACT).createProduct(newProductName, otherProducts, newProductUnit, newProductAmount, ratioToProduct, owner, expirydateChild);
 
-        address newProduct = Database(DATABASE_CONTRACT).createProduct(newProductName, parentProduct1, newProductUnit, newProductAmount, ratioToProduct, owner);
-
-        for (uint k = 0; k < parentProduct1.length; k++){
-          Product pro2 = Product(parentProduct1[k]);
+        for (uint k = 0; k < otherProducts.length; k++){
+          Product pro2 = Product(otherProducts[k]);
           pro2.setAmount(pro2.getAmount() - (newProductAmount * ratioToProduct[k])) ; 
           pro2.collaborateInMerge(newProduct,ratioToProduct[k]);
           if (pro2.getAmount() == 0) pro2.setConsumed(true);  
@@ -419,6 +465,6 @@ contract Product {
 
 }
 
-//db  0x7Ec2C09E1439114fcD487D94391f37553FfC0d38
-//ac2 0xEeeC1ebdbdE22Ca3b50FCEC8F39d39C6814a92A2
-//ac3 0x6e68295d05BF5cAe966792ADf9b5Ac8ca284dF99
+//db  0x1461514A707A7F3A4aB61fd421C8b5734c136631
+//ac2 0xb7aa436D720Dc881C6C7bD68668Ae41De896C9bc
+//ac3 0x914eCd4226C84011A5fE24415bEb4f1089873660
